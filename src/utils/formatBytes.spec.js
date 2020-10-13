bd69b8f7a38737aa48811f258bc210d8af1ad678
dd8888 <daviddiazh97@gmail.com>
2020-10-13 22:12:22 +0100

    Implement kata that simulates a leaderboard

--- src/utils/formatBytes.spec.js
+++ src/utils/formatBytes.spec.js
@@ -1,28 +1,4 @@
-const formatBytes = require("./formatBytes");
-
-/*const value1 = formatBytes(900);
-console.log(value1); // 900MB
-const value2 = formatBytes(1900);
-console.log(value2); // 1TB 900MB
-const value3 = formatBytes(568200);
-console.log(value3); // 568GB 200MB
-const value4 = formatBytes(1234567);
-console.log(value4); // 1TB 234GB 567MB
-
-assertEquals(formatBytes(900), "900MB");
-assertEquals(formatBytes(1900), "1GB 900MB");
-assertEquals(formatBytes(568200), "568GB 200MB");
-assertEquals(formatBytes(12345678), "12TB 345GB 678MB");
-
-function assertEquals(result, expectedResult) {
-  if (result !== expectedResult) {
-    throw new Error(
-      "First test failed: Expected " + expectedResult + ", got " + result,
-    );
-  }
-}
-console.log("All ok");
-*/
+import formatBytes from "./formatBytes";
 
 describe("formatBytes", () => {
   it("formats in MBs", () => {
--- src/utils/moneyFormat.spec.js
+++ src/utils/moneyFormat.spec.js
@@ -1,21 +1,4 @@
-const moneyFormat = require("./moneyFormat");
-
-/*const firstResult = moneyFormat(19.99);
-const secondResult = moneyFormat(250);
-const thirdResult = moneyFormat(9500.95);
-
-assertEquals(firstResult, "$19.99");
-assertEquals(secondResult, "$250.00");
-assertEquals(thirdResult, "$9500.95");
-
-function assertEquals(result, expectedResult) {
-  if (result !== expectedResult) {
-    throw new Error(
-      "First test failed: Expected " + expectedResult + ", got " + result,
-    );
-  }
-}
-console.log("All ok");*/
+import moneyFormat from "./moneyFormat";
 
 describe("moneyFormat", () => {
   it("Works with two decimals", () => {
